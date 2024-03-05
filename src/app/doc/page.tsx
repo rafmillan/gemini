bc9e808ef36d8a44896d265eb183ff70300c0339
rafmillan <rafael.k.millan@gmail.com>
2024-03-05 14:53:41 -0800

    added WIP for /doc

--- src/app/doc/page.tsx
+++ src/app/doc/page.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import { MdConstruction } from "react-icons/md";
 
 
 export default function Chat() {
   return (
-    <div>
-      document q&a
+    <div className="h-dvh dt:h-full w-full flex flex-col items-center justify-center">
+      <MdConstruction size={128} className="text-gray-500"/>
+      <h1 className="text-gray-500 font-medium tracking-wide text-3xl">Under Construction</h1>
     </div>
   )
 }
